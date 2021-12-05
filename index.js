d7737ba338379502152655f02c6ba5bb5ce7bcce
Tanja Rietveldt <diafox2015@gmail.com>
2021-12-05 21:49:34 +0100

    add separ func in a loop to create cards

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
+let log = console.log
 const app = document.getElementById('app')
 //create nav bar
 const nav = document.createElement('div')
 // create main container
 const main = document.createElement('div')
-let log = console.log
-
-
 app.appendChild(nav)
 nav.textContent = 'De schone slaapster '
 nav.className = 'nav-style'
@@ -14,30 +12,60 @@ app.appendChild(main)
 main.className = 'main-style'
 main.textContent = 'Main content'
 
-//card
+const  data = [
+    {  "id" : 1,
+        "title" :  "roze wolk",
+        "price"  : "$4.99"
+    } ,
+    {    "id" : 2,
+        "title" : "green meer",
+        "price" :"$234.99"
+    } ,
+    {   "id" : 3,
+        "title" : "blue umbrella",
+        "price" :"$14.99"
+    }  ,
+    {     "id"  : 4,
+        "title" : "sweet pado",
+        "price" : "$5.99"
+    }
+]
+const prods = data
 
-let card = document.createElement('div')
-main.appendChild(card)
-card.className = 'card-style'
-// card.textContent ='card content'
+// create function to make
+    let cardCreation = (arr)=> {
+        arr.forEach((item, index) => {
+            let card = document.createElement('div')
+            main.appendChild(card)
+            card.className = 'card-style'
 
-//img for card
-let cardImg = document.createElement('img')
-cardImg.src = 'img/roze.jpg'
-cardImg.className = 'card-img'
-card.appendChild(cardImg)
+    let createCardImg = (item)=> {
+                let cardImg = document.createElement('img')
+                cardImg.src = 'img/roze.jpg'
+                cardImg.className = 'card-img'
+                card.appendChild(cardImg)
+            }
+       createCardImg(item)
+      let createCardTitle = (item)=> {
+                let cardTitle = document.createElement('h3')
+                cardTitle.className = 'cart-title'
+                cardTitle.textContent = item.title
+                card.appendChild(cardTitle)
+            }
+            createCardTitle(item)
+     let createCardPrice = (item)=> {
+                let cardPrice = document.createElement('h4')
+                cardPrice.textContent = `Price: ${item.price}`
+                cardPrice.className = 'card-price'
+                card.appendChild(cardPrice)
+            }
+    createCardPrice(item)
 
-//card title
-let cardTitle = document.createElement('h3')
-cardTitle.className = 'card-title'
-cardTitle.textContent ='Roze wolkjes'
-card.appendChild(cardTitle)
+    })
 
-// card price
-let cardPrice = document.createElement('h4')
-cardPrice.textContent ='Price: $4.99'
-cardPrice.className ='card-price'
-card.appendChild(cardPrice)
+
+}
+cardCreation(prods)
 
 
 
